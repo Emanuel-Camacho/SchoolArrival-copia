79e6b513616286cc7df085f223e78213c025ad81
TommyGun1510 <bytomas135@gmail.com>
2026-05-17 12:50:52 -0300

    .

--- Infraestructure/Data/EfRepository.cs
+++ Infraestructure/Data/EfRepository.cs
@@ -11,6 +11,8 @@ namespace SchoolArrival.Infrastructure.Data;
 public class EfRepository<T> : RepositoryBase<T> where T : class //EfRepository es un repo generico que sirve para reutilizar code para cualquier entidad
 //codigo mas legible y ordenado, no repite code: sin heredar todos los repos. tiene todos los metdos, usas solo los que necesites. 
 {
+    // baja la base de datos y la guarda en _context
+    // protected sirve para no estar repitiendo codigo y que las clases hijas de EfRepository puedan acceder al context / BD
     protected readonly TravelArrivalDbContext _context;
     public EfRepository(TravelArrivalDbContext dbContext) : base(dbContext)
     {
